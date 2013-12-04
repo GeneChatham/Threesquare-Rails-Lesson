b9ca9e8d7edb9d56c94e0605ae38b1d350be090d
GeneChatham <genechatham@gmail.com>
2013-12-04 15:01:22 -0600

    Added edit functionality.  Working on Destroy functionality.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,9 @@
 class Location < ActiveRecord::Base
+
+	validates :name, :picture_url, :review, presence: true
+
+	def snippet
+		self.review.truncate(25)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,15 @@ Threesquare::Application.routes.draw do
   root 'locations#index'
   resources :locations
 end
+
+
+#        Prefix Verb   URI Pattern                   Controller#Action
+#          root GET    /                             locations#index
+#     locations GET    /locations(.:format)          locations#index
+#               POST   /locations(.:format)          locations#create
+#  new_location GET    /locations/new(.:format)      locations#new
+# edit_location GET    /locations/:id/edit(.:format) locations#edit
+#      location GET    /locations/:id(.:format)      locations#show
+#               PATCH  /locations/:id(.:format)      locations#update
+#               PUT    /locations/:id(.:format)      locations#update
+#               DELETE /locations/:id(.:format)      locations#destroy
